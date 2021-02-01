7c1d8f46b2efd3185cb75835e608b97063b9170c
Suprateem Banerjee <dolphinextreme48@gmail.com>
2021-02-01 17:55:23 +0530

    Add files via upload

--- PID.cpp
+++ PID.cpp
@@ -37,8 +37,8 @@ void PID::UpdateError(double cte, double prev_cte = 0, double total_cte = 0)
    * TODO: Update PID errors based on cte.
    */
   p_error = Kp * cte;
-  d_error = cte - prev_cte;
-  i_error = total_cte;
+  d_error = Kd * (cte - prev_cte);
+  i_error = Ki * total_cte;
 }
 
 double PID::TotalError() 
